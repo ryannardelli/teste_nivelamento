2f22259d2e452a1564647aa91d4654e44e415c45
Ryan <ryannarelli12@gmail.com>
2025-03-28 23:59:48 -0300

    enviando ajustes do servidor vue

---
+++ parte_4/interface_web/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Data from './components/Data.vue'
+</script>
+
+<template>
+  <div>
+    <Data />
+  </div>
+</template>
---
+++ parte_4/interface_web/src/components/Data.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Dados do CSV:</h1>
+</template>
